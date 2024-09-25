57b7ba22d5b30e3cf6fa6c92dc109e42af72154d
Eric Mesa <ericsbinaryworld@gmail.com>
2024-09-24 20:12:11 -0400

    Will read a playlist file and will get similar artists from last.fm

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # sdpmpd - Smart Dynamic Playlists for MPD
 
+config.json v1:
+```json
+{
+  "last_fm_key": "something",
+  "last_fm_secret": "something",
+  "playlist_location": "path_to_playlists"
+}
+```
+
+playlist file v1:
+
+```json
+{
+  "tag_type": "eg: album, artist, tag, etc",
+  "value": "the name of the artist or album or tag",
+  "strict": true,
+  "similar": true
+}
+```
+Create a playlist at the past in your config. For now while there isn't a package:
+
+Go into the sdpmpd folder and launch with:
+
+python main.py name_of_playlist
+
+
 Phases
-- dynamic playlists
-- Using last.fm and/or spotify to enable "similar artist" mode
-- smart, dynamic playlists
+- dynamic playlists <- done
+- anything better than time.sleep to check for playlist changes in the loop?
+- Using last.fm and/or spotify to enable "similar artist" mode <- done
+- smart, dynamic playlists <- done
+- use Pydantic to validate the playlist files
+- prevent songs already on the playlist from showing up, unless it's been a long time since they were played.
 - complex, smart, and dynamic playlists (ie using AND or NOT)
+- Create GUI
--- sdpmpd/main.py
+++ sdpmpd/main.py
@@ -1,5 +1,9 @@
 from mpd import MPDClient
 from random import choice
+from xdgenvpy import XDGPedanticPackage
+from json import loads
+import sys
+import requests
 
 def build_playlist(our_client: MPDClient, tag_type: str, search_term: str, strict: bool = False)->list[dict]:
     """Build a playlist that will be used to dynamically feed the MPD server.
@@ -11,24 +15,58 @@ def build_playlist(our_client: MPDClient, tag_type: str, search_term: str, stric
     else:
         return our_client.search(tag_type, search_term)
 
+import pathlib
 if __name__ == '__main__':
+    xdg = XDGPedanticPackage('sdpmpd')
+    config = loads(pathlib.Path(f'{xdg.XDG_CONFIG_HOME}/config.json').read_text())
+    last_fm_api = config.get('last_fm_key')
+    last_fm_secret = config.get('last_fm_secret')
+    playlist_file = sys.argv[1]
+    with open(f'{config["playlist_location"]}/{playlist_file}') as playlist_file_handle:
+        try:
+            playlist_parameters = loads(playlist_file_handle.read())
+        except FileNotFoundError:
+            sys.exit()
     client = MPDClient()
     client.connect("localhost", 6600)
-    playlist = build_playlist(client, "artist", "Anberlin")
-    # check to see if playlist is empty
-    status = client.status()
-    if status['playlistlength'] == '0':
-        # seed with 2 songs
-        client.add(choice(playlist)['file'])
-        client.add(choice(playlist)['file'])
-    while True:
+    if not playlist_parameters.get("similar"):
+        playlist = build_playlist(client, playlist_parameters.get("tag_type"), playlist_parameters.get("value"), playlist_parameters.get("strict"))
         status = client.status()
-        if int(status['song']) >= int(status['playlistlength'])-1:
-            client.add(choice(playlist)['file'])  # will eventually want to make sure not adding a song currently on the playlist or at least not within the last X number of songs.
-    print(client.status())
-    #for song in playlist:
-    #    client.add(song['file'])
-    #print(client.currentsong())
-    #print(client.find("any", "Relient K"))
-    #print(client.status())
+        if status['playlistlength'] == '0':
+            # seed with 2 songs
+            client.add(choice(playlist)['file'])
+            client.add(choice(playlist)['file'])
+        while True:
+            status = client.status()
+            if int(status['song']) >= int(status['playlistlength']) - 1:
+                client.add(choice(playlist)[
+                               'file'])  # will eventually want to make sure not adding a song currently on the playlist or at least not within the last X number of songs.
+    else:
+        payload = {"artist": playlist_parameters.get("value"), "api_key": last_fm_api, "format":"json"}
+        request = requests.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar", params=payload)
+        similar_artists_result = request.json()
+        similar_artists_list = similar_artists_result.get('similarartists').get('artist')
+        similar_artists = [artist.get('name') for artist in similar_artists_list]
+        similar_artists.append(playlist_parameters.get("value"))
+        # start off with the artist we searched for
+        playlists = []
+        for artist in similar_artists:
+            playlist = build_playlist(client, "artist", artist)
+            if playlist: # make sure we have a song by this artist
+                playlists.append(playlist)
+        # check to see if playlist is empty
+        status = client.status()
+        if status['playlistlength'] == '0':
+            # seed with 2 songs
+            chosen_playlist = choice(playlists)
+            client.add(choice(chosen_playlist)['file'])
+            chosen_playlist = choice(playlists)
+            client.add(choice(chosen_playlist)['file'])
+            client.play()
+        while True:
+            status = client.status()
+            if int(status['song']) >= int(status['playlistlength'])-1:
+                chosen_playlist = choice(playlists)
+                client.add(choice(chosen_playlist)['file']) # will eventually want to make sure not adding a song currently on the playlist or at least not within the last X number of songs.
+        print(client.status())
     client.disconnect()
